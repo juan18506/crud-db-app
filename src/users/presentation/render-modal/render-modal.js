d6df4a1806121dc37d697bdd45d0803aa2a05b16
juan18506 <juan185067@gmail.com>
2023-10-14 01:01:56 -0300

    modal form added validations

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -10,6 +10,7 @@ export const showModal = () => {
 
 export const hideModal = () => {
   modal?.classList.add('hide-modal');
+  form?.reset();
 }
 
 /**
@@ -35,5 +36,24 @@ export const renderModal = (element) => {
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    const formData = new FormData(form);
+    const userLike = {};
+
+    for (const [key, value] of formData) {
+      if (key === 'balance') {
+        userLike[key] = +value;
+        continue;
+      }
+
+      if (key === 'isActive') {
+        userLike[key] = value === 'on';
+        continue
+      }
+
+      userLike[key] = value;
+    }
+
+    hideModal();
   });
 }
