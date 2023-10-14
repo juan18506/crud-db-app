63361db39f8ed075e6096b1af604bc643372b030
juan18506 <juan185067@gmail.com>
2023-10-14 03:26:51 -0300

    implemented delete button

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from '../../store/users-store';
+import { deleteUserById } from '../../use-cases/delete-user-by-id';
 import { showModal } from '../render-modal/render-modal';
 import './render-table.css';
 
@@ -38,6 +39,26 @@ const tableSelectListener = (event) => {
 
 /**
  * 
+ * @param {MouseEvent} event 
+ */
+const tableDeleteListener = async (event) => {
+  const element = event.target.closest('.delete-user');
+  if (!element) return;
+
+  const id = element.getAttribute('data-id');
+  try {
+    await deleteUserById(id);
+    await usersStore.reloadPage();
+    document.querySelector('#current-page').innerText = usersStore.getCurrentPage();
+    renderTable();
+  } catch (error) {
+    console.log(error);
+    alert('Could not be deleted');
+  }
+}
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
 export const renderTable = (element) => {
@@ -48,6 +69,7 @@ export const renderTable = (element) => {
     element.append(table);
 
     table.addEventListener('click', tableSelectListener);
+    table.addEventListener('click', tableDeleteListener);
   }
 
   let tableHTML = '';
