b43a591527ab1bd9758ebf4a6ff9398f900edc9d
juan18506 <juan185067@gmail.com>
2023-10-14 01:43:51 -0300

    created getUserById and loaded user info on select

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from '../../store/users-store';
+import { showModal } from '../render-modal/render-modal';
 import './render-table.css';
 
 let table;
@@ -25,6 +26,18 @@ const createTable = () => {
 
 /**
  * 
+ * @param {MouseEvent} event 
+ */
+const tableSelectListener = (event) => {
+  const element = event.target.closest('.select-user');
+  if (!element) return;
+
+  const id = element.getAttribute('data-id');
+  showModal(id);
+}
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
 export const renderTable = (element) => {
@@ -34,24 +47,24 @@ export const renderTable = (element) => {
     table = createTable();
     element.append(table);
 
-    //TODO: listeners
+    table.addEventListener('click', tableSelectListener);
   }
 
   let tableHTML = '';
   users.forEach((user) => {
     tableHTML += `
-    <tr>
-      <td>${user.id}</td>
-      <td>${user.balance}</td>
-      <td>${user.firstName}</td>
-      <td>${user.lastName}</td>
-      <td>${user.isActive}</td>
-      <td>
-        <a href="#/" data-id="${user.id}">Select</a>
-        |
-        <a href="#/" data-id="${user.id}">Delete</a>
-      </td>
-    </tr>
+      <tr>
+        <td>${user.id}</td>
+        <td>${user.balance}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.isActive}</td>
+        <td>
+          <a href="#/" class="select-user" data-id="${user.id}">Select</a>
+          |
+          <a href="#/" class="delete-user" data-id="${user.id}">Delete</a>
+        </td>
+      </tr>
     `;
 
     table.querySelector('tbody').innerHTML = tableHTML;
