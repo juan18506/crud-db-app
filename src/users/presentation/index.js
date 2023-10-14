c930180d652e31f5728dba3c1d88c29de98e0382
juan18506 <juan185067@gmail.com>
2023-10-14 00:25:21 -0300

    added add button

---
+++ src/users/presentation/index.js
@@ -0,0 +1,3 @@
+export * from './render-add-button/render-add-button';
+export * from './render-buttons/render-buttons';
+export * from './render-table/render-table';
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,6 +1,5 @@
-import { renderButtons } from "./presentation/render-buttons/render-buttons";
-import { renderTable } from "./presentation/render-table/render-table";
-import usersStore from "./store/users-store";
+import { renderTable, renderButtons, renderAddButton } from './presentation';
+import usersStore from './store/users-store';
 
 /**
  * 
@@ -13,4 +12,5 @@ export const usersApp = async (element) => {
 
   renderTable(element);
   renderButtons(element);
+  renderAddButton(element);
 }
