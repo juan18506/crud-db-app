48bfa3f1c460ac4a4691219ed406d88461e16331
juan18506 <juan185067@gmail.com>
2023-10-13 23:47:45 -0300

    updated loadNextPage fn

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -7,4 +7,6 @@ import usersStore from "./store/users-store";
 export const usersApp = async (element) => {
   element.innerHTML = 'Loading...';
   await usersStore.loadNextPage();
+
+  console.log(usersStore.getUsers());
 }
